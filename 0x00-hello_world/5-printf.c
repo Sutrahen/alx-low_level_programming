27c20ceb4ad500bb000710ebcbabd0f311151d67
Sutrahen <emmanuelurieto@gmail.com>
2022-09-08 21:44:18 -0700

    fixing things

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
  *
  * printf - prints a string
  *
- * Return - Always 0 (Success)
+ * Return : Always 0 (Success)
 */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("%s","with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
