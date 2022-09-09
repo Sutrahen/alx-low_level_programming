3218063740fd1fbaa9a03c9d6594d86537f44b95
Sutrahen <emmanuelurieto@gmail.com>
2022-09-08 21:49:37 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
+ * main - main block
  *
  * printf - prints a string
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
-	printf("%s","with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
