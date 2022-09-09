a78684ca0ed4fdccf56f47b0214a80556d20d9f4
Sutrahen <emmanuelurieto@gmail.com>
2022-09-08 21:04:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
-int main()
+
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+*/
+int main(void)
 {
-	puts("programming is like building a multilingual puzzle:\n");
+	puts("programming is like building a multilingual puzzle\n");
 	return (0);
 }
