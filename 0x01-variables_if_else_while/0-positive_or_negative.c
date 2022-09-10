0bb2decdefa3cd67c8deb9fe422bd2401a4c9cf5
Sutrahen <emmanuelurieto@gmail.com>
2022-09-10 13:33:02 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
 /**
- * main: main code block
+ * main: main block
  *
  * srand: returns a random variable 
  *
