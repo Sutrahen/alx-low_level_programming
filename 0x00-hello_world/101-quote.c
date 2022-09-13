d3ac6ab5422baf6b8bb38cd049f76bb136d17e16
Sutrahen <emmanuelurieto@gmail.com>
2022-09-12 18:39:55 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 1 if success
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" -Dora Korper, 2015-10-19\n", 59);
-	return (1)
+	write(2, "and that piece of art is useful\" -Dora Korper, 2015-10-19\n", 58);
+	return (1);
 }
