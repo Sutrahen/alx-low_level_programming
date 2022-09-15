ec6410b685b4c78b558e90f24f994ea63bac7ea2
Sutrahen <emmanuelurieto@gmail.com>
2022-09-14 19:25:44 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: the integer accepted by the function
+ *
+ * Description: outputs the value of the last digit of a a number
+ *
+ * Return: int
+*/
+int print_last_digit(int n)
+{
+	if (n = 56)
+		return (n);
+}
