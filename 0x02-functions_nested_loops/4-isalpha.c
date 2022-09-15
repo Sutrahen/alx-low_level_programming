51dfdbe28a9c8b14c3c03434e790e8b6a1167ddc
Sutrahen <emmanuelurieto@gmail.com>
2022-09-14 18:35:34 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,18 +3,16 @@
 /**
  * _isalpha - Entry point
  *
- * @c - the integer value it receives
+ * @c: the integer value it receives
  *
  * Return: 1 if true. 0 if false
 */
 int _isalpha(int c)
 {
-	int i = 'a';
-	
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (c == i)
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	}
-	return (0);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
