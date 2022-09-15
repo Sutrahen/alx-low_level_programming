d3a98ade2e966f7cb15904022211d23d1e1e3ba5
Sutrahen <emmanuelurieto@gmail.com>
2022-09-15 00:50:13 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,30 +9,32 @@
 */
 void times_table(void)
 {
-	int row, column, product, tens, ones;
+	int rone, cone, d;
 
-	for (row = 0; column <= 9; column++)
+	for (rone = 0; rone <= 9; rone++)
 	{
-		product = row * column;
-		tens = product / 10;
-		ones = product % 10;
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
 
-		if (column == 0)
-			_putchar('0');
-		else if (product < 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(ones + '0');
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(tens + '0');
-			_putchar(ones + '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
