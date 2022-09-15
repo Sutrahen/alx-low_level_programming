85f70221842fa992653e1be0d8d16fef1bc8e2d5
Sutrahen <emmanuelurieto@gmail.com>
2022-09-14 17:33:55 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,20 +3,23 @@
 /**
  * main - Entry point
  *
+ * print_alphabet - prints out the alphabet in lower case
+ *
  * _putchar - outputs character
  *
  * Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
-	char str[] = "a";
+	char small[] = "a";
 	int n = 0;
 	
-
-	while (str[n] <= 'z')
+	while (small[n] <= 'z')
 	{
-		_putchar(n);
-		n++;
+		char l = small[n];
+
+		_putchar(l);
+		l++;
 	}
 	_putchar('\n');
 }
