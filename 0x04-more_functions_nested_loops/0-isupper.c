fd865967ebd781fddb3629a269abbdbbc0ec9ad3
Sutrahen <emmanuelurieto@gmail.com>
2022-09-16 18:34:36 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks the code for upeer or lower case
+ *
+ * @c: the character it accepts
+ *
+ * Return: 1 if True, 0 if false
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
