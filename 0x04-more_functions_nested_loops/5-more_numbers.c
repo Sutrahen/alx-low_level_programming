8790610cd05dcfd6d6803d5f096523d8550c0192
Sutrahen <emmanuelurieto@gmail.com>
2022-09-16 19:49:50 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,16 @@
 */
 void more_numbers(void)
 {
+	char k = 0;
 	char i = 0;
 
 	while (i <= 10)
 	{
-		char k = 0;
 		while (k <= 14)
+		{
 			_putchar('0' + 14);
+			_putchar('\n');
+		}
+		i++;
 	}
-	i++;
-	_putchar('\n');
 }
