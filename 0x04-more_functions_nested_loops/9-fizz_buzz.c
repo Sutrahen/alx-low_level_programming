c8ef1a4e8197ac7dec66862338f51e806f4bada7
Sutrahen <emmanuelurieto@gmail.com>
2022-09-17 00:57:00 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -19,7 +20,7 @@ int main(void)
 			printf("Buzz");
 		else
 			printf("%d", i);
+		i++;
 	}
-	i++;
-	putchar('\n');
+	_putchar('\n');
 }
