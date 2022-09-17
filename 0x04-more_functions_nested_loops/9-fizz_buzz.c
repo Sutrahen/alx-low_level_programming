ae7a24caa8c99de8a8ac58a3a0072cbf3fdfafec
Sutrahen <emmanuelurieto@gmail.com>
2022-09-17 01:06:18 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,21 @@
 */
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
+		if (i % 3 == 0 && i != 0)
+			printf(" Fizz");
+		else if (i % 5 == 0 && i != 0)
+			printf(" Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else if (i== 1)
 			printf("%d", i);
-		i++;
+		else
+			printf(" %d", i);
 	}
-	_putchar('\n');
+	printf("\n");
+	return (0);
 }
