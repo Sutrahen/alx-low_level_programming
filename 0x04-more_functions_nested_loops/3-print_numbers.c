5e253be1f6eb57544b130299415f4a6c38fe5d6e
Sutrahen <emmanuelurieto@gmail.com>
2022-09-16 18:57:13 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - prints values
+ *
+ * Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+}
