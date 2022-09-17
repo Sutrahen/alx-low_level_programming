d75c104edeae4e8932c181d3a6077b4486945496
Sutrahen <emmanuelurieto@gmail.com>
2022-09-16 19:37:23 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers
+ * Return:0
+*/
+void more_numbers(void)
+{
+	char i = 0;
+
+	while (i <= 10)
+	{
+		char k = 0;
+		while (k <= 14)
+			_putchar('0' + 14);
+	}
+	i++;
+	_putchar('\n');
+}
