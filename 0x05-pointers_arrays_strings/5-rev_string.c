4ca7f30c589240504d2e68fe2b21ec979673691b
Sutrahen <emmanuelurieto@gmail.com>
2022-09-20 16:59:11 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	for (len = 0; s[len] != '\0'; len++)
 	i = 0;
 	half = len / 2;
-		
+
 	while (half--)
 	{
 		temp = s[len - i - 1];
@@ -21,5 +21,5 @@ void rev_string(char *s)
 		s[i] = temp;
 		i++;
 	}
-			
+
 }
