9a3fa3fccffd48b59b413469a45b373624a51736
Sutrahen <emmanuelurieto@gmail.com>
2022-09-22 09:31:41 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,8 +11,8 @@ char *string_toupper(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] > 96 && str[i] < 123)
-			str[i] -= 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
 		i++;
 	}
 	return (str);
