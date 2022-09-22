fb30dbcd76231be11e80c25fd02a69537a29fffc
Sutrahen <emmanuelurieto@gmail.com>
2022-09-22 07:54:33 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,21 +6,21 @@
  * @dest: string to be overwritten
  * @n: number of values to concatenate
  * Description: Concatenates n mumber of values from src to dest
- * Return: dest
+ * Return: string
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	i = 0;
 	while (dest[i] != '\0')
 		i++;
-	for (j = 0; j < n; j++)
+	while (src[j] != '\0' && n > j)
 	{
 		dest[i] = src[j];
+		j++;
 		i++;
 	}
-	if (dest[i -1] != '\0')
+	if (n > 0)
 		dest[i] = '\0';
 	return (dest);
 }
