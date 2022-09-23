08fba7ca62f82ec5922af6dda19b7dd28c556d35
Sutrahen <emmanuelurieto@gmail.com>
2022-09-22 18:33:02 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,19 +7,18 @@
 */
 char *leet(char *s)
 {
-	int i = 0, j = 0;
-	char array_leet[] = {'4', '3', '1', '0', '7'};
-	char array_up[] = {'A', 'E', 'L', 'O', 'T'};
-	char array_low[] = {'a', 'e', 'l', 'o', 't'};
-	
-	while (s[i] != '\0')
+	int i, j;
+	char *replace;
+
+	replace = "aAeEoOtTlL4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 5; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (s[i] == array_low[j] || s[i] == array_up[j])
-				s[i] = array_leet[j];
+			if (s[i] == replace[j])
+				s[i] = replace[j + 10];
 		}
-		i++;
 	}
 	return (s);
 }
