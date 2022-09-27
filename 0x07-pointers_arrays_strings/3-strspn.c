81ecee92c65b2769fe0176cf374f2b2f9763b956
Sutrahen <emmanuelurieto@gmail.com>
2022-09-27 09:10:24 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,13 +14,13 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] > '\0'; j++)
 		{
-			if(s[i] == accept[j])
+			if (s[i] == accept[j])
 			{
 				cmpt++;
 				break;
 			}
 		}
-		if(accept[j] == '\0')
+		if (accept[j] == '\0')
 			break;
 	}
 	return (cmpt);
