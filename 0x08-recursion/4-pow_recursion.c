e7005d72af4137acb33dd0217eddf48816f92365
Sutrahen <emmanuelurieto@gmail.com>
2022-09-28 19:40:10 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - search a string for any of a set of bytes
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in 's' that matches one of the byes in 'accept'
+ * or Nulll if no such byte is found
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
