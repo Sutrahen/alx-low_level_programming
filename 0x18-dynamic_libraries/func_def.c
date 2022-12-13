3096e98ddb0329a94894097e09a37bd79c896b3a
Sutrahen <emmanuelurieto@gmail.com>
2022-12-12 23:29:44 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/func_def.c
@@ -0,0 +1,61 @@
+#include "header.h"
+
+/**
+ * add - report sum of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of two nuumbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - report difference of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: difference of two numbers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - report product of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Product of two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - report quotient of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: quotient of two numbers
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - report remainder of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: remainder of two numbers
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
